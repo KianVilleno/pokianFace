4dc9e2bd132121d0fd0bb0267291c47df3020614
Kian <villenokian>
2021-04-21 18:09:45 +1000

    making the structure of ecommerce app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Switch, Link } from "react-router-dom";
+import "./App.css";
+
+import ShopPage from "./components/pages/shop/shop.component";
+import HomePage from "./components/pages/homepage/homepage.component";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import "./directory.styles.scss";
+import MenuItem from "../menu-item/menu-item.component"
+
+class Directory extends React.Component {
+
+  constructor () {
+    super();
+    this.state = {
+      sections: [
+        {
+          id: 1,
+          linkUrl: "hats",
+          title: "hat",
+          imageUrl : "https://images.unsplash.com/photo-1569520045266-66f21ad8a4be?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+        },
+        {
+          id: 2,
+          linkUrl: "earrings",
+          title: "earrings"
+        },
+        {
+          id: 3,
+          linkUrl: "shoes",
+          title: "shoes"
+        },
+        {
+          id: 4,
+          linkUrl: "hat",
+          title: "lol"
+        },
+        {
+          id: 5,
+          linkUrl: "hat",
+          title: "tf"
+        },
+        
+      ]
+    }
+  }
+
+  render () {
+    return (
+      <div className="directory-menu">
+        {
+          this.state.sections.map(({id,...sectionProp}) => 
+            (
+              <MenuItem  key={id} {...sectionProp} />
+            ))
+        }
+      </div>
+    )
+  }
+
+}
+
+export default Directory
